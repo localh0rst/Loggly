deab2f635ec1676cae6e9aba2f10c647ef7b6d99
localh0rst <git@fail.ninja>
2024-05-14 10:22:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Loggly - It's new $module
+
+# SYNOPSIS
+
+    use Loggly;
+
+# DESCRIPTION
+
+Loggly is ...
+
+# LICENSE
+
+Copyright (C) localh0rst.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+localh0rst <git@fail.ninja>
---
+++ lib/Loggly.pm
@@ -0,0 +1,39 @@
+package Loggly;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Loggly - It's new $module
+
+=head1 SYNOPSIS
+
+    use Loggly;
+
+=head1 DESCRIPTION
+
+Loggly is ...
+
+=head1 LICENSE
+
+Copyright (C) localh0rst.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+localh0rst E<lt>git@fail.ninjaE<gt>
+
+=cut
+
